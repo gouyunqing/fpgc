62f57304fda439b62d80028a2dfdbeacf859ad84
maggiewang027 <94302872+maggiewang027@users.noreply.github.com>
2021-12-12 18:37:43 +0000

    Update Fetch.hs

--- fpgc/src/Fetch.hs
+++ fpgc/src/Fetch.hs
@@ -5,10 +5,11 @@ module Fetch
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 
-type URL = String
+-- Produce the URL for the API call from a movie id.
+-- E.g. for movie_id = 550, the URL will be "https://api.themoviedb.org/3/movie/550?api_key=ed416751bdc0f9f32680e2c327ebf6a4".
 
-download :: URL -> IO L8.ByteString
-download url = do
-    request <- parseRequest url
+download :: Int -> IO L8.ByteString
+download movie_id = do
+    request <- parseRequest $ concat ["https://api.themoviedb.org/3/movie/", show movie_id, "?api_key=ed416751bdc0f9f32680e2c327ebf6a4"]
     response <- httpLBS request
     return $ getResponseBody response
