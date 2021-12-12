7efe00d1d3b255546e9461d214dcab9d331e9e5f
maggiewang027 <94302872+maggiewang027@users.noreply.github.com>
2021-12-12 18:54:58 +0000

    Update Fetch.hs

--- fpgc/src/Fetch.hs
+++ fpgc/src/Fetch.hs
@@ -7,7 +7,6 @@ import Network.HTTP.Simple
 
 -- Produce the URL request for a given movie ID, then download the URL.
 -- E.g. for movie_id = 550, the URL will be "https://api.themoviedb.org/3/movie/550?api_key=ed416751bdc0f9f32680e2c327ebf6a4".
-
 download :: Int -> IO L8.ByteString
 download movie_id = do
     request <- parseRequest $ concat ["https://api.themoviedb.org/3/movie/", show movie_id, "?api_key=ed416751bdc0f9f32680e2c327ebf6a4"]
