2cca224a36ffcc4d8c27f27e891fc0c9685d2381
maggiewang027 <94302872+maggiewang027@users.noreply.github.com>
2021-12-12 18:46:22 +0000

    Update Fetch.hs

--- fpgc/src/Fetch.hs
+++ fpgc/src/Fetch.hs
@@ -5,7 +5,7 @@ module Fetch
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 
--- Produce the URL for the API call from a movie id.
+-- Produce the URL request for a given movie ID, then download the URL.
 -- E.g. for movie_id = 550, the URL will be "https://api.themoviedb.org/3/movie/550?api_key=ed416751bdc0f9f32680e2c327ebf6a4".
 
 download :: Int -> IO L8.ByteString
