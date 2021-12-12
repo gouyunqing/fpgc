f9d8ea3173957deb1d03d05ea98261f119584dac
maggiewang027 <94302872+maggiewang027@users.noreply.github.com>
2021-12-12 17:12:20 +0000

    Update Fetch.hs

--- fpgc/src/Fetch.hs
+++ fpgc/src/Fetch.hs
@@ -1,3 +1,16 @@
-module Fetch where
+module Fetch
+    ( download
+    ) where
 
 import Types
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Simple
+
+type URL = String
+
+download :: URL -> IO L8.ByteString
+download url = do
+    request <- parseRequest url
+    response <- httpLBS request
+    return $ getResponseBody response
